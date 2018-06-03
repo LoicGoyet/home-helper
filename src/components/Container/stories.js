01ab078fb7e75b414a821eb457fd4b5cc28339ab
LoicGoyet <loic.goyet@gmail.com>
2018-06-03 21:03:57 +0200

    create <Container> component

---
+++ src/components/Container/stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Container from '../Container';
+
+const stories = storiesOf('Container', module);
+stories.add('default', () => (
+  <Container>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae expedita dolores, repudiandae debitis quod
+    magnam aliquam eligendi doloremque nam, dicta veritatis fugiat eaque nesciunt, ad itaque sed. Beatae, veritatis vel.
+  </Container>
+));
+
+export default stories;
