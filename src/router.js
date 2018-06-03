c84e52b378c64e779f70bda8df3a2d5921a69d16
LoicGoyet <loic.goyet@gmail.com>
2018-06-03 19:59:04 +0200

    create view todos

--- src/router.js
+++ src/router.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 import App from './components/App';
+import Todos from './views/Todos';
 
 const Router = () => (
   <Provider store={store}>
     <BrowserRouter>
       <div>
         <Route exact path="/" component={App} />
+        <Route exact path="/todos" component={Todos} />
       </div>
     </BrowserRouter>
   </Provider>
---
+++ src/views/Todos/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todos = () => <div>todos !</div>;
+
+Todos.defaultProps = {
+  children: undefined,
+};
+
+export default Todos;
