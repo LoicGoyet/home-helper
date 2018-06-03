06fcb86ca543a241c56eea6963b2e23b637b2ccd
LoicGoyet <loic.goyet@gmail.com>
2018-06-03 19:32:17 +0200

    fix #1 : redux installation and create first simple ducks

--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import App from './components/App';
 
 const Router = () => (
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={App} />
-    </div>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+      </div>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Router;
