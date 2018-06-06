934d46a659c6d6f881ac34f193c361e3113b4f7c
Loïc Goyet <loic.goyet@gensdeconfiance.fr>
2018-06-06 13:24:12 +0200

    Fix lint warnings

--- src/components/IngredientsList/index.js
+++ src/components/IngredientsList/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import COLORS from '../../style/colors';
-
 const IngredientsList = ({ ingredients, className }) => (
   <List className={className}>
     {ingredients.map(({ title, quantity }) => (
