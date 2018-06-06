55608dcfb8537af6fd515b01c143fa2d2d1bc8dc
LoicGoyet <loic.goyet@gmail.com>
2018-06-06 22:34:37 +0200

    Display undone tasks by categories

--- src/components/TasksList/index.js
+++ src/components/TasksList/index.js
@@ -3,14 +3,37 @@ import PropTypes from 'prop-types';
 
 import Task from '../../container/Task';
 
-const TasksList = ({ tasks }) => <div>{tasks.map(task => <Task task={task} key={`task / ${task.id}`} />)}</div>;
+const TasksList = ({ tasksSections, doneTasks }) => (
+  <React.Fragment>
+    {tasksSections.length > 0 &&
+      tasksSections.map(section => (
+        <React.Fragment key={section.category}>
+          <h2>{section.category}</h2>
+          {listTasks(section.tasks)}
+        </React.Fragment>
+      ))}
+
+    {tasksSections.length > 0 && doneTasks.length > 0 && <hr />}
+
+    {doneTasks.length > 0 && listTasks(doneTasks)}
+  </React.Fragment>
+);
+
+const listTasks = tasks => <div>{tasks.map(task => <Task task={task} key={`task / ${task.id}`} />)}</div>;
 
 TasksList.propTypes = {
-  tasks: PropTypes.arrayOf(PropTypes.object),
+  doneTasks: PropTypes.arrayOf(PropTypes.object),
+  tasksSections: PropTypes.arrayOf(
+    PropTypes.shape({
+      category: PropTypes.string.isRequired,
+      tasks: PropTypes.arrayOf(PropTypes.object),
+    })
+  ),
 };
 
 TasksList.defaultProps = {
-  tasks: [],
+  doneTasks: [],
+  tasksSections: [],
 };
 
 export default TasksList;
--- src/container/TodosList/index.js
+++ src/container/TodosList/index.js
@@ -1,9 +1,41 @@
 import { connect } from 'react-redux';
 
 import TasksList from '../../components/TasksList';
+import { isIn } from '../../utils/arrays';
 
-const mapStateToProps = state => ({
-  tasks: state.todos.tasks.sort((taskA, taskB) => taskA.done - taskB.done),
-});
+const mapStateToProps = state => {
+  const undoneTasks = state.todos.tasks.filter(task => !task.done);
+  const doneTasks = state.todos.tasks.filter(task => task.done);
+
+  const tasksSections = undoneTasks.reduce((sections, task) => {
+    const categories = sections.map(section => section.category);
+
+    if (!isIn(categories, task.category)) {
+      return [
+        ...sections,
+        {
+          category: task.category,
+          tasks: [task],
+        },
+      ];
+    }
+
+    return [
+      ...sections.map(section => {
+        if (section.category !== task.category) return section;
+
+        return {
+          ...section,
+          tasks: [...section.tasks, task],
+        };
+      }),
+    ];
+  }, []);
+
+  return {
+    tasksSections,
+    doneTasks,
+  };
+};
 
 export default connect(mapStateToProps)(TasksList);
