112466f4c8c2cc1c51d559da2592c98b611902a7
LoicGoyet <loic.goyet@gmail.com>
2018-07-08 19:01:58 +0200

    display <Button> and ingredient list in <RecipesList> container

--- src/container/RecipesList/index.js
+++ src/container/RecipesList/index.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import { connect } from 'react-redux';
 
 import RecipesList from '../../components/RecipesList';
+import Button from '../../components/Button';
 
 const mapStateToProps = state => ({
-  recipes: state.recipes.collection.map(({ title, tags, ...recipe }) => ({
+  recipes: state.recipes.collection.map(({ title, tags, id, ingredients }) => ({
     title,
     tags,
+    mainBtn: (
+      <Button onClick={e => e.stopPropagation()} block>
+        Add
+      </Button>
+    ),
+    ingredients: ingredients.map(ingredient => ({
+      title: ingredient.title,
+      quantity: {
+        number: ingredient.quantity,
+        unit: ingredient.quantityUnit,
+      },
+    })),
   })),
 });
 
