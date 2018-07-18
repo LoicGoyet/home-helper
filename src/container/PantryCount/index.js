4e5b810916693e0e4120e3b37d8d844fd2b365c9
LoicGoyet <loic.goyet@gmail.com>
2018-07-18 21:56:48 +0200

    fix bug that happens when pantry is deleted

--- src/container/PantryCount/index.js
+++ src/container/PantryCount/index.js
@@ -2,19 +2,21 @@ import { connect } from 'react-redux';
 
 import PantryCount from '../../components/PantryCount';
 
-const mapStateToProps = state => ({
-  tags: state.recipes.pantry.filter(recipe => !recipe.done).reduce((count, item) => {
-    const tags = { ...count };
-    // console.log(recipe);
-    return item.recipe.tags.reduce(
-      (acc, tag) => ({
-        ...acc,
-        [tag]: (acc[tag] || 0) + 1,
-      }),
-      tags
-    );
-  }, {}),
-  pantryLength: state.recipes.pantry.filter(recipe => !recipe.done).length,
-});
+const mapStateToProps = state => {
+  const pantry = state.recipes.pantry || [];
+  return {
+    tags: pantry.filter(recipe => !recipe.done).reduce((count, item) => {
+      const tags = { ...count };
+      return item.recipe.tags.reduce(
+        (acc, tag) => ({
+          ...acc,
+          [tag]: (acc[tag] || 0) + 1,
+        }),
+        tags
+      );
+    }, {}),
+    pantryLength: pantry.filter(recipe => !recipe.done).length,
+  };
+};
 
 export default connect(mapStateToProps)(PantryCount);
