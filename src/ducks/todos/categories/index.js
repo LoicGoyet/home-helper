44b6d1ac9abb2bd4d0a042a93a17723d1d05db57
LoicGoyet <loic.goyet@gmail.com>
2018-08-19 17:01:16 +0200

    create todos sub ducks for categories

---
+++ src/ducks/todos/categories/index.js
@@ -0,0 +1,109 @@
+import { takeLatest, select, put } from 'redux-saga/effects';
+
+import Config from '../../../config';
+import database from '../../../utils/database';
+
+export const FETCH = 'home-helper/todos/categories/FETCH';
+export const FETCH_SUCCESS = 'home-helper/todos/categories/FETCH_SUCCESS';
+export const ADD_CATEGORY = 'home-helper/todos/categories/ADD_CATEGORY';
+export const UPDATE_CATEGORY = 'home-helper/todos/categories/UPDATE_CATEGORY';
+
+// Default state
+export const defaultState = {
+  byId: {},
+  allIds: [],
+};
+
+// Reducer
+// Actions
+const reducer = (state = defaultState, action = {}) => {
+  switch (action.type) {
+    case FETCH_SUCCESS: {
+      return action.data;
+    }
+
+    case ADD_CATEGORY: {
+      const id = state.allIds.length;
+      const { title } = action;
+      const createdAt = Date.now();
+
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [id]: {
+            id,
+            title,
+            createdAt,
+            updatedAt: createdAt,
+          },
+        },
+        allIds: [...state.allIds, id],
+      };
+    }
+
+    case UPDATE_CATEGORY: {
+      const { id, title } = action;
+
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [id]: {
+            ...state.byId[id],
+            title,
+            updatedAt: Date.now(),
+          },
+        },
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
+
+export default reducer;
+
+// Action Creators
+
+export const addCategory = title => ({
+  type: ADD_CATEGORY,
+  title,
+});
+
+export const updateCategory = (id, title) => ({
+  type: UPDATE_CATEGORY,
+  id,
+  title,
+});
+
+export const fetch = () => ({
+  type: FETCH,
+});
+
+// Sagas
+
+function* fetchCategory() {
+  if (Config.USE_MOCK) return yield;
+
+  const data = yield database
+    .ref('/todos/categories')
+    .once('value')
+    .then(snapshot => snapshot.val());
+
+  yield put({ type: FETCH_SUCCESS, data });
+}
+
+function* saveCategories() {
+  if (Config.USE_MOCK) return yield;
+  const { categories } = yield select(state => state.todos);
+  yield database.ref('/todos/categories').set(categories);
+}
+
+export function* todosSaga() {
+  yield takeLatest(FETCH, fetchCategory);
+  yield takeLatest(ADD_CATEGORY, saveCategories);
+  yield takeLatest(UPDATE_CATEGORY, saveCategories);
+}
