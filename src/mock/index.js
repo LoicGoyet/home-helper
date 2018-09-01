498b373f1483a0b637e90054369f1016d192ded8
LoicGoyet <loic.goyet@gmail.com>
2018-09-01 16:26:04 +0200

    add more data in mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -8,19 +8,19 @@ let mockIsLoaded = false;
 
 export default () => {
   if (!Config.USE_MOCK || mockIsLoaded) return;
-  // store.dispatch(addTask("Jus d'Orange", 'Boissons', 1, 'piece'));
-  // store.dispatch(addTask('Croissants', 'Boulangerie', 4, 'piece'));
-  // store.dispatch(addTask('Coca-Cola Zero', 'Boissons', 2, 'piece'));
-  // store.dispatch(addTask('Baguette', 'Boulangerie', 1, 'piece'));
-  // store.dispatch(addTask('Thon', 'Épicerie salée', 140, 'grams'));
-  // store.dispatch(addTask('Croûtons', 'Épicerie salée', 50, 'grams'));
-  // store.dispatch(addTask('Céréales', 'Épicerie sucrée', 500, 'grams'));
-  // store.dispatch(addTask('Salade Batavia', 'Fruits & légumes', 1, 'piece'));
-  // store.dispatch(addTask('Tomates', 'Fruits & 2mes', 3, 'piece'));
-  // store.dispatch(addTask('Pain de mie', 'pièce(s)', 14, 'piece'));
-  // store.dispatch(addTask('Pizza', 'pièce(s) Frais', 2, 'piece'));
-  // store.dispatch(addTask('Blanc de Poulet', 'Produits frais', 2, 'piece'));
-  // store.dispatch(addTask('Émincés de Poulet', 'Produits frais', 150, 'grams'));
+  store.dispatch(addTask("Jus d'Orange", 'Boissons', 1, 'piece'));
+  store.dispatch(addTask('Croissants', 'Boulangerie', 4, 'piece'));
+  store.dispatch(addTask('Coca-Cola Zero', 'Boissons', 2, 'piece'));
+  store.dispatch(addTask('Baguette', 'Boulangerie', 1, 'piece'));
+  store.dispatch(addTask('Thon', 'Épicerie salée', 140, 'grams'));
+  store.dispatch(addTask('Croûtons', 'Épicerie salée', 50, 'grams'));
+  store.dispatch(addTask('Céréales', 'Épicerie sucrée', 500, 'grams'));
+  store.dispatch(addTask('Salade Batavia', 'Fruits & légumes', 1, 'piece'));
+  store.dispatch(addTask('Tomates', 'Fruits & 2mes', 3, 'piece'));
+  store.dispatch(addTask('Pain de mie', 'pièce(s)', 14, 'piece'));
+  store.dispatch(addTask('Pizza', 'pièce(s) Frais', 2, 'piece'));
+  store.dispatch(addTask('Blanc de Poulet', 'Produits frais', 2, 'piece'));
+  store.dispatch(addTask('Émincés de Poulet', 'Produits frais', 150, 'grams'));
 
   store.dispatch(
     addInCollection(
@@ -36,8 +36,8 @@ export default () => {
         {
           productTitle: 'Pomme de terre',
           categoryTitle: 'Fruits & légumes',
-          quantity: 2,
-          unitTitle: 'pièce(s)',
+          quantity: 200,
+          unitTitle: 'grammes',
         },
       ]
     )
@@ -724,9 +724,9 @@ export default () => {
     )
   );
 
-  // store.dispatch(addPantryEntry(0));
-  // store.dispatch(addPantryEntry(1));
-  // store.dispatch(addPantryEntry(1));
+  store.dispatch(addPantryEntry(0));
+  store.dispatch(addPantryEntry(1));
+  store.dispatch(addPantryEntry(1));
 
   mockIsLoaded = true;
 };
