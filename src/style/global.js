53f4344ea49bfa892e03ffda09ddd9559aa648b5
LoicGoyet <loic.goyet@gmail.com>
2018-09-14 23:27:32 +0200

    set html font-size

--- src/style/global.js
+++ src/style/global.js
@@ -5,6 +5,10 @@ import COLORS from './colors';
 injectGlobal`
     @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700');
 
+    html {
+      font-size: 14px;
+    }
+
     body {
         margin: 0;
         font-family: 'Roboto', sans-serif;
