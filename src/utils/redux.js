41c6f2bf7e0e8644adde740a563882ed766b2f99
Loïc Goyet <loic.goyet@gmail.com>
2019-12-08 23:03:50 +0100

    update way to generate id

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -4,11 +4,5 @@
  */
 export const generateId = allIds => {
   if (allIds.length === 0) return 0;
-  return Math.max(...allIds) + 1;
+  return allIds.length;
 };
-
-/**
- * Retrieve the last id created in state
- * @param {number[]} allIds collection of all the ids registered in state
- */
-export const getLastIdGenerated = allIds => Math.max(...allIds);
